01c2a35a0cbb7a3ea50f59ac48c588878a2a142f
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-09 22:45:03 -0500

    init commit. add basic landing page.

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/** Stateless widget
+ * a widget with no direct state management. it only renders the value/data from the state
+ */
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // paper widget used to build UI's
+    return new Material(
+      color: Colors.greenAccent,
+      // button widget
+      child: new InkWell(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              "Let's Quizzz",
+              style: new TextStyle(
+                fontSize: 48.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            new Text(
+              "Tap to start!",
+              style: new TextStyle(
+                fontSize: 32.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            new Text(""),
+          ],
+        ),
+      ),
+    );
+  }
+}
