5bea9bc873c1664afddf4cce80961fcaecdd5d8e
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-21 18:13:29 -0500

    add animation to question label.

--- lib/ui/question_label.dart
+++ lib/ui/question_label.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class QuestionText extends StatefulWidget {
-
   // widget variables
   final String _question;
   final int _questionNumber;
@@ -13,7 +12,30 @@ class QuestionText extends StatefulWidget {
   State createState() => new QuestionTextState();
 }
 
-class QuestionTextState extends State<QuestionText> {
+class QuestionTextState extends State<QuestionText>
+    with SingleTickerProviderStateMixin {
+  // SingleTicker... helps with animation
+  // configured to only tick while the current tree is enabled
+
+  Animation<double> _fontSizeAnimation;
+  AnimationController _fontSizeAnimationController;
+
+  // init state method
+  @override
+  void initState() {
+    super.initState();
+    _fontSizeAnimationController = new AnimationController(
+        duration: new Duration(milliseconds: 500),
+        vsync: this // takes in widget (with the SingleTickerProvider)
+        );
+    _fontSizeAnimation = new CurvedAnimation(
+      parent: _fontSizeAnimationController,
+      curve: Curves.bounceOut,
+    );
+    _fontSizeAnimation.addListener(() => this.setState(() {}));
+    _fontSizeAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Material(
@@ -22,7 +44,13 @@ class QuestionTextState extends State<QuestionText> {
         padding: new EdgeInsets.symmetric(vertical: 20.0),
         child: new Center(
           // NOTE: Use keyword, 'widget' to access the class variables in the Widget class
-          child: new Text("Statement " + widget._questionNumber.toString() + ": " + widget._question),
+          child: new Text(
+            "Statement " +
+                widget._questionNumber.toString() +
+                ": " +
+                widget._question,
+            style: new TextStyle(fontSize: _fontSizeAnimation.value * 15), // increases from 15 * 0.01 to 15 * 1.00
+          ),
         ),
       ),
     );
