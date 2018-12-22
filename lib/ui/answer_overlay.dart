c3661b515ed4cf269554af6150529c592f704cd8
Sam McCagg <sam.mccagg@ellucian.com>
2018-12-22 16:59:03 -0500

    add answer overlay animation.

--- lib/ui/answer_overlay.dart
+++ lib/ui/answer_overlay.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class AnswerOverlay extends StatefulWidget {
   final bool _isCorrect;
@@ -8,7 +9,26 @@ class AnswerOverlay extends StatefulWidget {
   AnswerOverlayState createState() => AnswerOverlayState();
 }
 
-class AnswerOverlayState extends State<AnswerOverlay> {
+class AnswerOverlayState extends State<AnswerOverlay>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+      duration: new Duration(seconds: 2,),
+      vsync: this,
+    );
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.elasticOut,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Material(
@@ -23,9 +43,13 @@ class AnswerOverlayState extends State<AnswerOverlay> {
                   color: Colors.white,
                   shape: BoxShape.circle,
                 ),
-                child: new Icon(
+                child: new Transform.rotate(
+                  angle: _iconAnimation.value * 2 * pi,
+                  child: new Icon(
                     widget._isCorrect == true ? Icons.done : Icons.close,
-                    size: 80.0),
+                    size: _iconAnimation.value * 80.0,
+                  ),
+                ),
               ),
               new Padding(
                 padding: new EdgeInsets.only(bottom: 20.0),
